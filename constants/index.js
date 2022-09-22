eecd2d9015c4d52817ccb6d6b346d29a65161bb4
marianif <47568942+marianif@users.noreply.github.com>
2022-09-22 17:21:01 +0200

    hardcoded production url in constants

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/"
-    : process.env.NEXT_PUBLIC_BASE_URL;
+    : "https://seetizen-frontend.vercel.app/";
