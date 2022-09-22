ff0097f99dc4c38f950369901f2cf37455a559a2
marianif <47568942+marianif@users.noreply.github.com>
2022-09-22 16:21:00 +0200

    moved pages folder outside of src

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import "../src/styles/globals.css";
+import { store } from "../src/store/store";
+import { Provider } from "react-redux";
+import Amplify from "aws-amplify";
+import config from "@/src/aws-exports";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "@/styles/theme";
+import PropTypes from "prop-types";
+import createEmotionCache from "@/src/createEmotionCache";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Amplify.configure({
+  ...config,
+  // aws_appsync_apiKey: props.AWS_API_KEY,
+  ssr: true,
+});
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
