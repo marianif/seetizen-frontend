bd4cc095815e97449fc6ffa4a206aae3ffcbd053
federica.mariani <federica.mariani@MBP-POLITI.lan>
2023-04-02 18:26:09 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,125 +1,147 @@
-import { useRouter } from "next/router";
-import styles from "@/styles/pages/Login.module.css";
-import { useState, useEffect } from "react";
-import { Layout } from "../src/components";
-import { Auth } from "aws-amplify";
-import { Spinner } from "../src/components";
-import { LoadingButton } from "@mui/lab";
-import { colors } from "@/utils/colors";
-import { BASE_URL } from "constants";
+import React, { useState, useEffect, useCallback } from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { Marker } from "@react-google-maps/api";
+import MapView from "@/components/Map/MapView";
+import { PROGRAM, HOME_BUTTONS } from "@/constants/index";
+import { PLACES } from "@/constants/places";
+import styles from "@/styles/pages/Map.module.css";
+import MapCards from "@/components/Map/MapCards/MapCards";
+import CronoCalendar from "@/components/Map/CronoCalendar/CronoCalendar";
+import Details from "@/components/Map/Details/Details";
+import Image from "next/image";
+import BannerCrowdfunding from "@/components/Map/BannerCrowfunding/BannerCrowdfunding";
+import EventBanner from "@/components/Map/EventBanner/EventBanner";
+import HomeButton from "@/components/Map/HomeButton/HomeButton";
+import HouseList from "@/components/Map/HouseList/HouseList";
+import ArtistList from "@/components/Map/ArtistList/ArtistList";
+import ArtistDetail from "@/components/Map/ArtistDetail/ArtistDetail";
+import HouseDetail from "@/components/Map/HouseDetail/HouseDetail";
+import MapBar from "@/components/MapBar/MapBar";
 
-const LoginPage = (props) => {
-  console.log(BASE_URL);
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const [values, setValues] = useState({
-    email: "",
-    password: "",
-  });
+import { API, graphqlOperation } from "aws-amplify";
+import { updateFlow } from "@/src/graphql/mutations";
+import { getFlow, listFlows } from "@/src/graphql/queries";
+import { onUpdateFlow } from "@/src/graphql/subscriptions";
+import { Api } from "@mui/icons-material";
+import { subscribe } from "graphql";
 
-  useEffect(() => {
-    // Accessing the user session on the client
-    Auth.currentAuthenticatedUser()
-      .then(() => {
-        router.push("/dashboard");
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
-  }, []);
+const SHOW = {
+  HOUSE_PROGRAM: "HOUSE PROGRAM",
+  DETAILS: "DETAILS",
+  ARTIST_LIST: "ARTIST_LIST",
+  ARTIST_DETAIL: "ARTIST_DETAIL",
+  HOUSE_DETAIL: "ARTIST_DETAIL",
+  MAP: "MAP",
+};
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
+const containerStyle = {
+  width: "100%",
+  height: "80%",
+};
+
+const center = {
+  lat: 45.0677551,
+  lng: 7.6824892,
+};
+
+const Map = ({ markers }) => {
+  const [show, setShow] = useState(SHOW.MAP);
+  const [flows, setFlows] = useState([]);
+
+  const [house, setHouse] = useState();
+  const [artist, setArtist] = useState();
+
+  const [item, setItem] = useState();
+
+  const onItemClick = (item) => {
+    setItem(item);
+    setShowDetails(true);
   };
 
-  const handleSubmit = async (e) => {
-    setLoading(true);
+  const onHousePress = (item) => {
+    setHouse(item.id);
+    setShow(SHOW.HOUSE_DETAIL);
+  };
+
+  const getFlowRate = async () => {
     try {
-      e.preventDefault();
-      const hasEmptyFields = Object.values(values).some(
-        (value) => value === ""
-      );
-
-      if (hasEmptyFields) {
-        setShowError(true);
-        alert("Compila tutti i campi del form");
-        return;
-      }
-      const { attributes } = await Auth.signIn({
-        username: values.email,
-        password: values.password,
-      });
-
-      if (attributes) {
-        router.push("/dashboard");
-      }
+      const { data } = await API.graphql(graphqlOperation(listFlows));
+      setFlows(data.listFlows.items);
     } catch (error) {
       console.warn(error);
     }
   };
 
+  useEffect(() => {
+    // Subscribe to creation of Todo
+    (async () => {
+      const updateSub = await API.graphql(
+        graphqlOperation(onUpdateFlow)
+      ).subscribe({
+        next: ({ provider, value }) => getFlowRate(), // Process incoming messages
+      });
+    })();
+    //return () => updateSub.unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    getFlowRate();
+  }, []);
+
   return (
-    <Layout headerType={"landing"}>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className={styles.container}>
-          <h3>Accedi alla tua dashboard</h3>
-          <p>
-            Accedi per pubblicare eventi e articoli direttamente dalla tua
-            dashboard
-          </p>
-          <div className={styles.formContainer}>
-            <form>
-              <span>
-                <input
-                  onChange={onChange}
-                  placeholder="Email"
-                  value={values.email}
-                  name="email"
-                  type="text"
-                  required
-                  inputMode="email"
-                />
-                <input
-                  onChange={onChange}
-                  placeholder="Password"
-                  value={values.password}
-                  name="password"
-                  type="password"
-                  required
-                  minLength={8}
-                  autoComplete="current-password"
-                />
-              </span>
-              <LoadingButton
-                laoding={loading}
-                onClick={handleSubmit}
-                sx={{
-                  backgroundColor: colors.emerald400,
-                  color: colors.white,
+    <div style={{ flex: 1, height: "100%" }}>
+      <div className={styles.page}>
+        <MapBar
+          onProgramPress={() => setShow(SHOW.HOUSE_PROGRAM)}
+          onMapPress={() => setShow(SHOW.MAP)}
+        />
+        {show === SHOW.MAP && (
+          <div
+            style={{
+              width: "100%",
+              //height: "100%",
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <MapView places={PLACES} onItemClick={onItemClick} />
+
+            {/* {HOME_BUTTONS.map((item, index) => (
+              <HomeButton
+                key={index}
+                item={item}
+                onClick={() => {
+                  index === 0
+                    ? setShow(SHOW.HOUSE_PROGRAM)
+                    : index === 1 && setShow(SHOW.ARTIST_LIST);
                 }}
-              >
-                Accedi
-              </LoadingButton>
-            </form>
+              />
+            ))} */}
           </div>
-          <div className={styles.innerBottom}>
-            <p>
-              Non hai un account?{" "}
-              <a href="" target={"_blank"} rel="noreferrer">
-                Scarica l&apos; app
-              </a>
-              , registrati e usa le tue credenziali per accedere alla dashboard
-            </p>
-          </div>
-        </div>
-      )}
-    </Layout>
+        )}
+        {show === SHOW.DETAILS && (
+          <Details place={item} onCloseClick={() => setShowDetails(false)} />
+        )}
+        {show === SHOW.HOUSE_PROGRAM && (
+          <HouseList
+            onClose={() => setShow(SHOW.MAP)}
+            onHousePress={onHousePress}
+            onExhbitionPress={() => {}}
+            flows={flows}
+          />
+        )}
+        {show === SHOW.ARTIST_LIST && (
+          <ArtistList onClose={() => setShow(SHOW.MAP)} />
+        )}
+        {show === SHOW.ARTIST_DETAIL && (
+          <ArtistDetail item={{}} onClose={() => setShow(SHOW.MAP)} />
+        )}
+        {show === SHOW.HOUSE_DETAIL && (
+          <HouseDetail item={{}} onClose={() => setShow(SHOW.MAP)} />
+        )}
+      </div>
+    </div>
   );
 };
 
-export default LoginPage;
+export default Map;
