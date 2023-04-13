0d52d9c907c405dfe1d6cf5dc1c047171a789580
federica.mariani <federica.mariani@MBP-POLITI.lan>
2023-04-13 16:07:43 +0200

    secure update flow

--- src/components/UpdateCard/UpdateCard.js
+++ src/components/UpdateCard/UpdateCard.js
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 
 const UpdateCard = ({ item, flows, onConfirm }) => {
   const [value, setValue] = useState(1);
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const onButtonPress = () => {
+    onConfirm(item.houseId, value);
+    setShowSuccess(true);
+    setTimeout(() => {
+      setShowSuccess(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    const current = flows.find((flow) => flow.id === item.houseId).value;
+    if (current === value) {
+      return;
+    }
+
+    if (current !== value) {
+      setValue(current);
+    }
+  }, []);
+
   return (
     <Box
       sx={{
@@ -12,11 +33,45 @@ const UpdateCard = ({ item, flows, onConfirm }) => {
         backgroundColor: "transparent",
         borderWidth: 1,
         padding: "5%",
+        border: "1px solid",
+        borderColor: "white",
+        marginBottom: 5,
+        borderRadius: 8,
       }}
     >
       <h2 style={{ color: "white", fontFamily: "Bebas Neue", fontSize: 50 }}>
         {item.name}
       </h2>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor:
+              value === 1
+                ? "green"
+                : value === 2
+                ? "yellow"
+                : value === 3 && "red",
+            width: 30,
+            height: 30,
+            aspectRatio: 1,
+            borderRadius: 100,
+            marginRight: 10,
+          }}
+        />
+        <p style={{ color: "white", fontFamily: "Bebas Neue", fontSize: 20 }}>
+          {value === 1
+            ? "Spazio libero"
+            : value === 2
+            ? "Si sta riempiendo"
+            : value === 3 && "Affollato"}
+        </p>
+      </Box>
       <Slider
         defaultValue={flows.find((flow) => flow.id === item.houseId).value}
         step={1}
@@ -26,18 +81,28 @@ const UpdateCard = ({ item, flows, onConfirm }) => {
         onChange={(e) => setValue(e.target.value)}
         color="secondary"
       />
+      {showSuccess && (
+        <h4
+          className="text-light"
+          style={{ alignSelf: "center", textAlign: "center" }}
+        >
+          Affluenza modificata con successo!
+        </h4>
+      )}
       <button
-        onClick={() => onConfirm(item.houseId, value)}
+        onClick={onButtonPress}
         style={{
-          backgroundColor: "transparent",
+          backgroundColor: "white",
           padding: 10,
-          borderWidth: 1,
-          borderRadius: 0,
-          borderColor: "white",
+          borderWidth: 2,
+          borderRadius: 10,
           marginTop: 20,
+          color: "black",
+          fontFamily: "Bebas Neue",
+          width: "100%",
         }}
       >
-        Conferma e Aggiorna
+        Modifica Affluenza
       </button>
     </Box>
   );
