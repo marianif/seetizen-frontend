20505f2533d18c64bf87c7c9950df08904659e45
federica.mariani <federica.mariani@MBP-POLITI.local>
2023-04-14 17:01:45 +0200

    proetc codee

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    CODE: process.env.AUTHORIZATION_CODE,
+    AUTHORIZATION_CODE: process.env.AUTHORIZATION_CODE,
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
 };
